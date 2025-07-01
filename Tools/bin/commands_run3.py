806f35e4f1ad39943f83aa18a67309b12380124b
christinaw97 <christina.wang@caltech.edu>
2025-07-01 16:18:17 -0700

    update 2024 json

--- Tools/bin/commands_run3.py
+++ Tools/bin/commands_run3.py
@@ -1,15 +1,18 @@
 import os
 
-version = 'v12'
-golden_json_path = "/storage/af/user/christiw/login-1/christiw/LLP/Run3/CMSSW_9_4_4/src/RazorCommon/Tools/data/Run3/"
+version = 'v39'
+golden_json_path = os.getenv("CMSSW_BASE") + "/src/RazorCommon/Tools/data/Run3/"
 samples = {
-#        '2022':"Muon-EXOCSCCluster_Run2023-PromptReco.root",
+        '2022':"DisplacedJet-EXOCSCCluster_Run2022-PromptReco",
         '2023':"Muon-EXOCSCCluster_Run2023-PromptReco",
-}
+        '2024':"Muon-EXOCSCCluster_Run2024-PromptReco",
+
+        }
 json = {
         '2022':'Cert_Collisions2022_355100_362760_Golden.json',
         '2023':'Cert_Collisions2023_366442_370790_Golden.json',
-}
+        '2024': 'Cert_Collisions2024_378981_386951_Golden.json',
+        }
 for year, sample in samples.items():
         directory="/storage/af/group/phys_exotica/delayedjets/displacedJetMuonAnalyzer/Run3/V1p19/Data{0}/{1}/normalized/".format(year, version)
         print(sample)
